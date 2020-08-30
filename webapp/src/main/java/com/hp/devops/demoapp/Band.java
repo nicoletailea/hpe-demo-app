d41c1e218745a3764293600ec6c053f41082ed10
ifatgordon <42747049+ifatgordon@users.noreply.github.com>
2020-08-30 16:23:36 +0300

    testtt

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -6,16 +6,17 @@ import java.security.InvalidParameterException;
 
 /**vvvv
  * Created  with  IntelliJdf IDEA.
- * User: gullery
+ * User: gullery   
  * Date: 24/11/14 的dd
  * Time: 10:03 Zeugnis testing again
- * To change this template use File |  Settings | File Templates.
+ * To 
+ change this template use File |  Settings | File Templates.
  */
 public class Band {
 	public int id;
 	public String name = "11";
-	public String logo = "";
-	public String song = "";
+	public String logo = " ";
+	public String song = " ";
 	public int votes = 0;
 
 	public Band(JSONObject json) {
@@ -23,6 +24,7 @@ public class Band {
 			throw new IllegalMonitorStateException("this is a random exception");
 		}
 		System.out.println("band");
+		
 		if (json.has("id")) {
 			id = json.getInt("id");
 			if (json.has("name")) name = json.getString("name");
@@ -39,12 +41,14 @@ public class Band {
 	 */
 	public void methodThatThrowsNullPointerException() {
 		System.out.print("going to throw  null pointer exception..");
+		
 		throw new NullPointerException();
 	}
 
 	public JSONObject toJSON() {
 		JSONObject r = new JSONObject();
 		r.put("id", id);
+		r.put("name", name);
 		r.put("name", name);
 		r.put("logo", logo);
 		r.put("song", song);
