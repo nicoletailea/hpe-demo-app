1d3619f405557a8852a14c1892de30c6aeda3618
sa@nga <sa@nga>
2020-10-22 20:14:09 +0300

    user story #1020

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertTrue;
  * this is new *asdwe
 =======
  * this is newrgtrdfd
->>>>>>> c00a1ac6278f44ae8e7dbdffc6a6538a717dcf0
+>>>>>>> c00a1ac6278f44ae8e7dbdffc6a6538a717dcf
  */
 public class CalcsTest {
 	////asssggaga
